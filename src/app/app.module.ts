9c3484ff51d49ae2fed2f2f8cf4e32b4252ef588
Bryan Blanchard <bryan.blanchard@gmail.com>
2017-04-08 13:57:09 -0400

    Create list of collapsible data views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { BannerComponent } from './banner/banner.component';
 import { ListItemComponent } from './list-item/list-item.component';
+import { ViewListComponent } from './view-list/view-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BannerComponent,
-    ListItemComponent
+    ListItemComponent,
+    ViewListComponent
   ],
   imports: [
     BrowserModule,
